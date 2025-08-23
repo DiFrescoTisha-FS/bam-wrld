453b8b926493b7e37e10847eebe7d1928ceb1c58
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2025-08-23 18:21:19 -0400

    Testing with console.log

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+console.log('REACT_APP_CLOUDINARY_CLOUD_NAME=', process.env.CLOUDINARY_CLOUD_NAME);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
